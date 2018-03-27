19a97fc5eef2d6fe4e5632cb921257ecac75865d
Matthew Mogavero <matt.moga@gmail.com>
2018-03-27 19:59:05 -0400

    refactored login page to use a stateful component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,20 +5,38 @@ import { login } from '../store';
 
 
 class Login extends Component {
-    render() {
+    constructor() {
+        super();
+        this.state ={
+            username: '',
+            password: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e){
+        const {username, password} = this.state;
+        e.preventDefault();
+        this.props.login(username, password);
+        this.props.history.push('/'); // send me home
+    }
+        render() {
         return (
             <div>
-                <form onSubmit={e => {
-                    e.preventDefault();
-                    this.props.login('testguy', 'password');
-                    this.props.history.push('/'); // send me home
-                    }}>
+                <form onSubmit={this.handleSubmit}>
                     <h2>Login:</h2>
                     <div>
-                        <input type="email" placeholder="Email..." required />
+                        <input type="email" 
+                            placeholder="Email..." 
+                            value={this.state.username}
+                            onChange={e => this.setState({username: e.target.value})}
+                            required />
                     </div>
                     <div>
-                        <input type="password" placeholder="Password..." required />
+                        <input type="password" 
+                            placeholder="Password..."
+                            value={this.state.password} 
+                            onChange={e => this.setState({password: e.target.value})}
+                            required />
                     </div>
                     <button>Login</button>
 
